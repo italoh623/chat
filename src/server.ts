23f1da44b9e21c742ca534dbcbdfff358be69769
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-04-21 02:28:39 -0300

    Adding Settings

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import express from "express";
 
+import  "./database";
+import { routes } from "./routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá NLW5"
-  })
-})
+app.use(express.json());
 
-app.post("/", (request, response) => {
-  return response.json({
-    message: "Usuário salvo com sucesso!"
-  })
-})
+app.use(routes);
 
 app.listen(3333, () => console.log("server is running on port 3333"));
