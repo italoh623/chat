d3399427613e566f1e43e8858068e791fef15d2e
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-04-22 11:59:51 -0300

    Save message and connection

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,8 +1,54 @@
 import { io } from "../http";
+import { ConnectionsService } from "../services/ConnectionsService";
+import { MessagesService } from "../services/MessagesService";
+import { UsersService } from "../services/UsersService";
+
+interface Iparams {
+  text: string;
+  email: string;
+}
 
 
 io.on("connect", (socket) => {
-  socket.on("client_first_acess", params => {
-    console.log(params)
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+  const messasgeService = new MessagesService();
+
+  socket.on("client_first_acess", async params => {
+    const socket_id = socket.id;
+    const { text, email } = params as Iparams;
+    let user_id = null;
+    
+    const userExists = await usersService.findByEmail(email);
+
+    if(!usersService) {
+      const user = await usersService.create(email);
+
+      await connectionsService.create({
+        socket_id,
+        user_id: user.id
+      });
+
+      user_id = user.id;
+    } else {
+      user_id = userExists.id;
+
+      const connection = await connectionsService.findUserById(userExists.id);
+
+      if (!connection) {
+        await connectionsService.create({
+          socket_id,
+          user_id: userExists.id
+        });
+      } else {
+        connection.socket_id = socket_id;
+        await connectionsService.create(connection);
+      }
+    }
+
+    await messasgeService.create({
+      text,
+      user_id
+    })
   })
 });
